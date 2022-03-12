b8a2e0ff58f8e36059f4575ebdab5fee1ff39e08
Juanjomza <94985512+Juanjomza@users.noreply.github.com>
2022-03-11 22:12:55 -0300

    Primer carga

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var nombre = String ()
+var apellido = String()
+var DNI = Number ()
+var Direccion = String + Number ()
+function Datos (nombre, apellido, DNI, Direccion)
+    
+
+console.log = function Datos
